30aa32910bb89d01ffc3df459a63821b97dc13fe
Josue Arce <josue.arce@webmechanix.com>
2019-04-15 23:40:09 -0400

    formatting spacing

--- qSearch.js
+++ qSearch.js
@@ -1,29 +1,30 @@
 class qSearch{
-	constructor(containerName, options){
+  
+  constructor(containerName, options){
     let qsContainer = `[data-qSearch-set=${containerName}]`; 
     let itemName = `[data-qSearch-item=${options.dataAttr}]`;
 
     this._container = document.querySelector(qsContainer);
     
-  	if(options.hasOwnProperty('dataAttr')){
-    	this._items = document.querySelectorAll(itemName);
+    if(options.hasOwnProperty('dataAttr')){
+      this._items = document.querySelectorAll(itemName);
     }
-  	
-   	this._data = Array.prototype.slice.call(this._items);
+    
+    this._data = Array.prototype.slice.call(this._items);
   }
   
   search(value){
     let list = this._data;
     let rxp = new RegExp(value, 'gi');
     let filtered = list.filter(item => rxp.test(item.innerText));
-    
+
     this.render(filtered);
   }
   
   render(newList){
-  	let updatedListGroup = this._container.querySelector('[data-qSearch-list]');
+    let updatedListGroup = this._container.querySelector('[data-qSearch-list]');
     let updatedList = newList.map(item => item.outerHTML).join('');
-    
+
     updatedListGroup.innerHTML = updatedList;
   }
 }
