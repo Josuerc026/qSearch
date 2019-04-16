4ca35d9938a3e2261c5bbce8bb27a6f6138773eb
Josue Arce <josue.arce@webmechanix.com>
2019-04-15 23:32:38 -0400

    init commit

---
+++ qSearch.js
@@ -0,0 +1,29 @@
+class qSearch{
+	constructor(containerName, options){  
+  
+  	let qsContainer = `[data-qSearch-set=${containerName}]`;
+    let itemName = `[data-qSearch-item=${options.dataAttr}]`;
+    
+  	if(options.hasOwnProperty('dataAttr')){
+    	this._items = document.querySelectorAll(itemName);
+    }
+  	
+  	this._container = document.querySelector(qsContainer);
+   	this._data = Array.prototype.slice.call(this._items);
+  }
+  
+  search(value){
+    let list = this._data;
+    let rxp = new RegExp(value, 'gi');
+    let filtered = list.filter(item => rxp.test(item.innerText));
+    
+    this.render(filtered);
+  }
+  
+  render(newList){
+  	let updatedListGroup = this._container.querySelector('[data-qSearch-list]');
+    let updatedList = newList.map(item => item.outerHTML).join('');
+    
+    updatedListGroup.innerHTML = updatedList;
+  }
+}
