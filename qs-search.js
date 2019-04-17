132b92a92ba891ce6718b1f591386ba09e82fc42
Josue Arce <josue.arce@webmechanix.com>
2019-04-17 00:26:28 -0400

    formatting & indenting

--- qs-search.js
+++ qs-search.js
@@ -3,21 +3,20 @@ import QSBase from './qs-base';
 class QSearch extends QSBase {
 
   constructor(container, options){
-  	super(container, options);
+      super(container, options);
   }
   
   search(value){
-  
-   	let rxp = new RegExp(value, 'gi');
-    let filtered = this._data.filter(item => rxp.test(item.innerText));
-    
-    this.render(filtered);
+      let rxp = new RegExp(value, 'gi');
+      let filtered = this._data.filter(item => rxp.test(item.innerText));
+      
+      this.render(filtered);
   }
   
   render(newList){
-    let updatedListGroup = this._container.querySelector('[data-qSearch-list]');
-    let updatedList = newList.map(item => item.outerHTML).join('');
-
-    updatedListGroup.innerHTML = updatedList;
+      let updatedListGroup = this._container.querySelector('[data-qSearch-list]');
+      let updatedList = newList.map(item => item.outerHTML).join('');
+      
+      updatedListGroup.innerHTML = updatedList;
   }
 }
