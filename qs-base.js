f0360a6a4d4c0a65c636b56498a28e8e819f57d9
Josue Arce <josue.arce@webmechanix.com>
2019-04-17 22:51:06 -0400

    Added multiple qs-item data support for list items

--- qs-base.js
+++ qs-base.js
@@ -1,40 +1,66 @@
 export default class QSBase {
-  
-  constructor(containerName, options){
-    this._container = document.querySelector(`[data-qSearch-set=${containerName}]`);
+
+  constructor(containerName, options) {
+    this._container = this.setContainer(containerName);
+    this._list = this.setList();
     this._data = this.dataHandler(options);
   }
-  
-  dataHandler(options){
-    if(!options.hasOwnProperty('data')) return;
 
-    if(typeof options.data == 'string'){
-      return this.setSingleData(options.data);
+  // Stores the qs-set container
+  setContainer(containerName) {
+    return document.querySelector(`[data-qs-set=${containerName}]`);
+  }
+
+  // Stores the qs-list within container
+  setList() {
+    return this._container.querySelector(`[data-qs-list]`);
+  }
+
+  // Handles type checking for the passed data in options
+  // continues only if data is an array
+  dataHandler(options) {
+    if (!options.hasOwnProperty('data')) return;
+
+    let data = options.data;
+
+    if (typeof data != 'object') {
+      data = this.convertToArray(data);
     }
-    
-    if(typeof options.data == 'object'){
-     return this.setMultipleData(options.data);
+
+    if (typeof data == 'object') {
+      return this.setData(data);
+    }else{
+      throw Error('data option is not an array.');
     }
   }
-  
-  setSingleData(data){
-    let itemName = `[data-qs-item=${data}]`;
-    this._items = document.querySelectorAll(itemName);
-    
-    return Array.prototype.slice.call(this._items);
+
+  // Converts data string to required array, if necessary
+  convertToArray(data) {
+    return data.split(' ');
   }
- 
-  setMultipleData(dataArray){
-    let list = this._container.querySelector(`[data-qs-list]`);
-    let allItems = Array.prototype.slice.call(list.children);
-    
-    this._data = allItems.map(elm => {
+
+  // Converts items in lists to readable data objects
+  setData(dataArray) {
+    let allItems = Array.prototype.slice.call(this._list.children);
+    let propertySetup = this.setDataProperties(dataArray);
+
+    return allItems.map(elm => propertySetup(elm));
+  }
+
+  // Object setup for each element in list by storing:
+  // * qs-item as key/value pair (e.g. <span data-qs-item="name">John<span> -> name: john)
+  // * compare property is the string that will be compared by the regExp engine
+  // * elm is the list item that will be stored to re-render
+  setDataProperties(dataArray) {
+    return (elm) => {
       let values = {};
-      for(let i = 0; i < dataArray.length; i++){
+
+      values.elm = elm;
+      for (let i = 0; i < dataArray.length; i++) {
         values[dataArray[i]] = elm.querySelector(`[data-qs-item=${dataArray[i]}]`).innerText;
+        values.compare = elm.innerText;
       }
-      
       return values;
-    });
+    }
   }
 }
--- qs-search.js
+++ qs-search.js
@@ -2,21 +2,18 @@ import QSBase from './qs-base';
 
 class QSearch extends QSBase {
 
-  constructor(container, options){
-      super(container, options);
-  }
-  
-  search(value){
-      let rxp = new RegExp(value, 'gi');
-      let filtered = this._data.filter(item => rxp.test(item.innerText));
-      
-      this.render(filtered);
-  }
-  
-  render(newList){
-      let updatedListGroup = this._container.querySelector('[data-qSearch-list]');
-      let updatedList = newList.map(item => item.outerHTML).join('');
-      
-      updatedListGroup.innerHTML = updatedList;
-  }
+    constructor(container, options) {
+        super(container, options);
+    }
+
+    search(value) {
+        let rxp = new RegExp(value, 'gim');
+        let filtered = this._data.filter(item => rxp.test(item.compare));
+
+        this.render(filtered);
+    }
+
+    render(newList) {
+        this._list.innerHTML = newList.map(item => item.elm.outerHTML).join('');
+    }
 }
