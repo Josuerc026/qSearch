5eb61f4585006f0f67308cde2fcf010882fa1a79
Josue Arce <josue.arce@webmechanix.com>
2019-05-02 23:00:26 -0400

    declaring compare string before storing key/value pairs - removed QBase

--- qs-search.js
+++ qs-search.js
@@ -1,9 +1,70 @@
-import QSBase from './qs-base';
+class QSearch {
 
-class QSearch extends QSBase {
+    constructor(containerName, options) {
+        this._container = this.setContainer(containerName);
+        this._list = this.setList();
+        this._data = this.dataHandler(options);
+    }
 
-    constructor(container, options) {
-        super(container, options);
+    // Stores the qs-set container
+    setContainer(containerName) {
+        return document.querySelector(`[data-qs-set=${containerName}]`);
+    }
+
+    // Stores the qs-list within container
+    setList() {
+        return this._container.querySelector(`[data-qs-list]`);
+    }
+
+    // Handles type checking for the passed data in options
+    // continues only if data is an array
+    dataHandler(options) {
+        if (!options.hasOwnProperty('data')) return;
+
+        let data = options.data;
+
+        if (typeof data != 'object') {
+            data = this.convertToArray(data);
+        }
+
+        if (typeof data == 'object') {
+            return this.setData(data);
+        } else {
+            throw Error('data option is not an array.');
+        }
+    }
+
+    // Converts data string to required array, if necessary
+    convertToArray(data) {
+        return data.split(' ');
+    }
+
+    // Converts items in lists to readable data objects
+    setData(dataArray) {
+        let allItems = Array.prototype.slice.call(this._list.children);
+        let propertySetup = this.setDataProperties(dataArray);
+
+        return allItems.map(elm => propertySetup(elm));
+    }
+
+    // Object setup for each element in list by storing:
+    setDataProperties(dataArray) {
+        return (elm) => {
+            let values = {};
+
+            // * elm is the list item element that will be stored to re-render
+            values.elm = elm;
+
+            // * compare property is the string that will be compared by search method
+            values.compare = elm.innerText;
+
+            // * qs-item as key/value pair (e.g. <span data-qs-item="name">John<span> -> name: john)
+            for (let i = 0; i < dataArray.length; i++) {
+                values[dataArray[i]] = elm.querySelector(`[data-qs-item=${dataArray[i]}]`).innerText;
+            }
+            
+            return values;
+        }
     }
 
     // Search data by using compare string defined in the items object
